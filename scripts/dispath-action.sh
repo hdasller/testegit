5c3f8947dcb4fd62553a28fa5f5180682b6fa586
Henrique Dasller <hdasller@gmail.com>
2021-07-21 20:36:58 -0300

    vf1

---
+++ scripts/dispath-action.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+curl 
+--location 
+--request GET 'https://api.github.com/repos/hdasller/testegit/actions/workflows/11506890/dispatches' \
+--header 'Authorization: Bearer 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' \
